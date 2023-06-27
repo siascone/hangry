2e562dcecb1ce0e4f288399de8934c6c8e5a8aff
siascone <spencer.iascone@gmail.com>
2023-06-27 16:08:34 -0600

    set map placeholder and adjust index scroll

--- frontend/src/components/Businesses/businessIndex.js
+++ frontend/src/components/Businesses/businessIndex.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllBusinesses } from "../../store/businesses";
 import BusinessIndexItem from "./businessIndexItem";
+import Map from "../Map/Map";
 import './business.css'
 
 function BusinessIndex () {
@@ -15,10 +16,13 @@ function BusinessIndex () {
     },[])
 
     return (
-        <div className="business-index-container">
-            {businesses.map((business, i) => {
-                return <BusinessIndexItem business={business} key={i}/>
-            })}
+        <div className="business-index-main">
+            <div className="business-index-container">
+                {businesses.map((business, i) => {
+                    return <BusinessIndexItem business={business} key={i}/>
+                })}
+            </div>
+            <Map />
         </div>
     )
 }
---
+++ frontend/src/components/Map/Map.js
@@ -0,0 +1,11 @@
+import './Map.css';
+
+function Map () {
+    return (
+        <div className="map-container">
+            Map coming soon...
+        </div>
+    )
+}
+
+export default Map;
