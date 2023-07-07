c95ecf25e066fd6a864d0acd885143d03564b0ec
siascone <spencer.iascone@gmail.com>
2023-07-07 13:57:08 -0600

    Get map working with markers. Markers need work as they don't all route to correct business page

--- frontend/src/components/Businesses/businessIndex.js
+++ frontend/src/components/Businesses/businessIndex.js
@@ -1,19 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchAllBusinesses } from "../../store/businesses";
 import BusinessIndexItem from "./businessIndexItem";
-import Map from "../Map/Map";
 import './business.css'
+import Map from "../Map/Map";
+import { useHistory } from "react-router-dom";
 
 function BusinessIndex () {
 
     const businesses = useSelector(state => Object.values(state.businesses))
-
+    const history = useHistory();
     const dispatch = useDispatch();
+    const [highlightedBusiness, setHighlightedBusiness] = useState(null);
+
 
     useEffect(() => {
         dispatch(fetchAllBusinesses())
     },[])
+
+    // const mapEventHandlers = useMemo(() => {
+    //     click: event => {
+    //         const search = new URLSearchParams(event.latLng.toJSONI().toString();
+    //         history.push({ pathname: 'businesses/'}))
+    //     }
+    // })
 
     return (
         <div className="business-index-main">
@@ -22,7 +32,16 @@ function BusinessIndex () {
                     return <BusinessIndexItem business={business} key={i}/>
                 })}
             </div>
-            <Map />
+            <Map 
+                businesses={businesses}
+                // mapEventHandlers={mapEventHandlers}
+                markerEventHandlers={{
+                    click: (business) => history.push(`/businesses/${business.id}`),
+                    mouseover: (business) => setHighlightedBusiness(business.id),
+                    mouseout: () => setHighlightedBusiness(null)
+                }}
+                highlightedBusiness={highlightedBusiness}
+            />
         </div>
     )
 }
--- frontend/src/components/Map/Map.js
+++ frontend/src/components/Map/Map.js
@@ -1,11 +1,94 @@
+import { useRef, useEffect, useState} from 'react';
 import './Map.css';
+import { Wrapper } from '@googlemaps/react-wrapper';
+import { useHistory } from 'react-router-dom';
 
-function Map () {
+function Map ({
+    businesses,
+    highlightedBusiness,
+    mapOptions = {},
+    mapEventHandlers = {},
+    markerEventHandlers = {}
+}) {
+    const [map, setMap] = useState(null);
+    const mapRef = useRef(null);
+    const markers = useRef({});
+    const history = useHistory();
+
+    useEffect(() => {
+        if (!map) {
+            setMap(new window.google.maps.Map(mapRef.current, {
+                center: {
+                    lat: 40.01820,
+                    lng: -105.28013
+                },
+                zoom: 14,
+                clickableIcons: false,
+                ...mapOptions,
+            }));
+        }
+    }, [mapRef, map, mapOptions]);
+
+    useEffect(() => {
+        if (map) {
+            // set markers
+            businesses.forEach(business => {
+                if (markers.current[business.id]) return;
+    
+                const marker = new window.google.maps.Marker({
+                    map,
+                    position: new window.google.maps.LatLng(business.latitude, business.longitude),
+                    // label: {
+                    //     text: `${business.name}`,
+                    //     fontWeight: 'bold',
+                    //     color: 'black'
+                    // }
+    
+                })
+    
+                Object.entries(markerEventHandlers).forEach(([event, handler]) => {
+                    marker.addListener(event, () => handler(business))
+                });
+    
+                markers.current[business.id] = marker
+            });
+    
+            // remove markers
+            Object.entries(markers.current).forEach(([businessId, marker]) => {
+                if (businesses.some(business => business.id.toString() === businessId)) return;
+    
+                marker.setMap(null);
+                delete markers.current[businessId]
+            })
+    
+        };
+            
+    }, [businesses, history, map, markerEventHandlers]);
+
+    // useEffect(() => {
+    //     if (map) {
+    //         const listeners = Object.entries(mapEventHandlers).map(([event, handler]) => window.google.maps.event.addListener(
+    //             map,
+    //             event, 
+    //             (...args) => handler(...args, map)
+    //         ));
+    //     }
+    // }, [map, mapEventHandlers]);
+
     return (
-        <div className="map-container">
-            Map coming soon...
+        <div className="map-container" ref={mapRef} >
+            Map
         </div>
     )
 }
 
-export default Map;
+function MapWrapper(props) {
+    console.log("key", process.env)
+    return (
+        <Wrapper apiKey={process.env.REACT_APP_MAPS_API_KEY}>
+            <Map { ...props} />
+        </Wrapper>
+    )
+}
+
+export default MapWrapper;
